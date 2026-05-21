71f0c70bafc4e50402450adf3bb85df42af57940
Bruno Macedo <devbrunomacedo@gmail.com>
2026-05-21 18:52:38 -0300

    (back end) | refactor(auth): mudanca na resposta para erros de validacao para as rotas de autenticacao

--- backend/src/modules/auth/middlewares/validate-schema.ts
+++ backend/src/modules/auth/middlewares/validate-schema.ts
@@ -4,11 +4,15 @@ import { ZodSchema } from "zod";
 export function validateSchema(schema: ZodSchema) {
     return (request: Request, response: Response, next: NextFunction) => {
         const result = schema.safeParse(request.body);
-
         if (!result.success) {
+            const errors = result.error.issues.map((issue) => ({
+                field: issue.path[0],
+                message: issue.message,
+            }));
+
             return response.status(400).json({
                 message: "Validation error",
-                errors: result.error.format(),
+                errors,
             });
         }
 
